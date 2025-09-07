20acd3f4014bcc867b7569d30bcee63d3ffe8a2a
yomex96 <abayomirobertonawole2@gmail.com>
2025-09-07 03:35:57 -0400

    test completed

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -254,3 +254,8 @@ class TestAccountService(TestCase):
             content_type="text/plain"
         )
         self.assertEqual(resp.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+    
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
